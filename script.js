f52381cfd6fb8cdc191711ac759ca90d6fddb957
PrtkPokhrel <pokhrelpratik52@gmail.com>
2024-08-19 19:22:25 +0545

    added some functionality in pricing sectin of the website, and it is still mobile base design first approach

--- script.js
+++ script.js
@@ -13,6 +13,34 @@
         })
     });
 
+
+
+    function updateButtonStyles(activeButton, inactiveButton) {
+        activeButton.style.backgroundColor = "#183D3D";
+        activeButton.style.color = 'white';
+        inactiveButton.style.backgroundColor = "#93B1A6";
+        inactiveButton.style.color = 'black';
+    }
+    
+    function monthly() {
+        let varMn = document.querySelector('.btn_1');
+        let varYr = document.querySelector('.btn_2');
+        
+        if (varMn && varYr) {
+            updateButtonStyles(varMn, varYr);
+        }
+    }
+    
+    function yearly() {
+        let varYr = document.querySelector('.btn_2');
+        let varMn = document.querySelector('.btn_1');
+        
+        if (varYr && varMn) {
+            updateButtonStyles(varYr, varMn);
+        }
+    }
+    
+
     // 780
     // 862
     //65956
